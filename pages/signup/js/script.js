19b968c6e17b7c4b0d6e0a948aecfc7e7e705908
kira? <79657238+mynameiskira@users.noreply.github.com>
2024-03-27 15:21:52 +0100

    Delete pages directory

--- pages/signup/js/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.signup-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    // Ici, vous pouvez ajouter la logique pour traiter les données du formulaire
-    alert('Formulaire soumis!');
-});
